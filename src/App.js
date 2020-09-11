9ce2105ce8cd4411dd1119a2eee639c8b54d2b67
Drk <wldnro3141@gmail.com>
2020-09-12 03:41:42 +0900

    update link

--- src/App.js
+++ src/App.js
@@ -9,35 +9,35 @@ function App() {
       backend : "SpringBoot",
       db : "MySQL",
       etc : "간단한 기능을 구현하되, 코드의 가독성에 집중하며 만든 프로젝트",
-      link : "/todolist-springboot"
+      link : "http://kjh-projects.kro.kr/todolist-springboot"
     },
     {
       title : "RecruitList",
       backend : "Spring",
       db : "MySQL, MongoDB",
       etc : "써보고 싶은 다양한 기술스택을 사용하며 만든 프로젝트",
-      link : "/recruitlist-spring"
+      link : "http://kjh-projects.kro.kr/recruitlist-spring"
     },
     {
       title: "SoldierMailAuto",
       backend : "미정",
       db: "미정",
       etc : "제작중",
-      link : "/soldier-mail-auto"
+      link : "http://kjh-projects.kro.kr/soldier-mail-auto"
     },
     {
       title: "MemorialDayCalender",
       backend: "미정",
       db: "미정",
       etc: "제작중",
-      link: "/memorialday-calender"
+      link: "http://kjh-projects.kro.kr/memorialday-calender"
     },
     {
       title: "When-Should-I-dodge",
       backend: "Django",
       db: "미정",
       etc: "제작중(롤 전적 검색을 기반으로 한 닷지 판단 웹)",
-      link: "/when-shuld-i-dodge"
+      link: "http://kjh-projects.kro.kr/when-shuld-i-dodge"
     }
   ];
 
